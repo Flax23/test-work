29f53263f3fb222bc4d35385a9f767db4e38bec0
ilia_sokolov <ilia_sokolov90@mail.ru>
2021-09-21 16:19:16 +0300

    123
    123

--- test/Assets/Scripts/DataBase.cs
+++ test/Assets/Scripts/DataBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using TMPro;
+
 
 public class DataBase : MonoBehaviour
 {
--- test/Assets/Scripts/UserManager.cs
+++ test/Assets/Scripts/UserManager.cs
@@ -2,49 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+
 
 public class UserManager : MonoBehaviour
 {
-    public TextMeshProUGUI output;
-    private Button button;
-    public GameObject buttonPrefab;
-    public DataBase dataBase;
 
-    // Start is called before the first frame update
-    void Start()
+    public RectTransform prefab;
+    public Text countText;
+    public RectTransform content;
+    //public DataBase dataBase;
+
+  
+    public void UpdateItems()
     {
-        dataBase = GameObject.Find("Load Data").GetComponent<DataBase>();
-        button = GetComponent<Button>();
-        //button.onClick.AddListener(SetUser);
-        ButtonPrefab();
+        //dataBase = GameObject.Find("Load Data").GetComponent<DataBase>();
+        int modelsCount = 0;
+        int.TryParse(countText.text, out modelsCount);
+        StartCoroutine(GetItems(modelsCount, results => OnReceivedModels(results)));
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnReceivedModels(TestItemModel[] models)
     {
-        
-    }
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
 
-    void SetUser()
-    {
-        foreach (var detail in dataBase.userDetail)
-        {   if (detail.userId == 0)
-            {
-                output.text = "Name: " + detail.name + "\r\n" + "Age: " + detail.age + "\r\n"
-                + "Relation: " + detail.relation;
-            }
-            if (detail.userId == 1)
-            {
-                output.text = "Name: " + detail.name + "\r\n" + "Age: " + detail.age + "\r\n"
-                + "Relation: " + detail.relation;
-            }
+        foreach (var model in models)
+        {
+            var instance = GameObject.Instantiate(prefab.gameObject) as GameObject;
+            instance.transform.SetParent(content, false);
+            InitializeItemView(instance, model);
         }
     }
 
-    void ButtonPrefab()
+    void InitializeItemView (GameObject viewGameObject, TestItemModel model)
     {
-        Instantiate(buttonPrefab, transform.position - new Vector3(0, -50.0f, 0), buttonPrefab.transform.rotation);
+        TestItemView view = new TestItemView(viewGameObject.transform);
+        view.titleText.text = model.title;
+        view.clickButton.GetComponentInChildren<Text>().text = model.buttonText;
+        view.clickButton.onClick.AddListener(
+            () =>
+            {
+                Debug.Log(view.titleText.text + "is clicked!");
+            }
+        );
     }
 
+    IEnumerator GetItems (int count, System.Action<TestItemModel[]> callback)
+    {
+        yield return new WaitForSeconds(1);
+        var result = new TestItemModel[count];
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = new TestItemModel();
+            result[i].title = "Item " + i;
+            result[i].buttonText = "Button " + i;
+        }
+
+        callback(result);
+    }
+
+    public class TestItemView
+    {
+        public Text titleText;
+        public Button clickButton;
+
+        public TestItemView(Transform rootView)
+        {
+            titleText = rootView.Find("TitleText").GetComponent<Text>();
+            clickButton = rootView.Find("ClickButton").GetComponent<Button>();
+        }
+    }
+
+    public class TestItemModel
+    {
+        public string title;
+        public string buttonText;
+    }
 }
