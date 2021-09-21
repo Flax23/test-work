94b0c08f714b2da8a6583683c9dca996fb144290
ilia_sokolov <ilia_sokolov90@mail.ru>
2021-09-21 19:07:17 +0300

    release
    release test

---
+++ test/Assets/Scripts/UsersDataBase.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
+public class UsersDataBase : MonoBehaviour
+{
+
+    public RectTransform prefab;
+    public Text details;
+    public RectTransform content;
+    public List<Users> userDetail = new List<Users>();
+
+    private void Start()
+    {
+        ReadJSON();
+        
+        foreach (var user in userDetail)
+        {
+            Debug.Log(user.name);
+            var instance = GameObject.Instantiate(prefab.gameObject) as GameObject;
+            instance.transform.SetParent(content, false);
+            InitializeUserView(instance, user);
+        }        
+    }
+
+    
+
+    void InitializeUserView(GameObject viewGameObject, Users user)
+    {
+        TestUserView view = new TestUserView(viewGameObject.transform);
+        view.titleText.text = "UserID: " + user.userId;
+        view.clickButton.GetComponentInChildren<Text>().text = "Show";
+        view.clickButton.onClick.AddListener(
+            () =>
+            {
+                details.text = "Name: " + user.name  + "\r\nAge: " + user.age + "\r\nRelation: " + user.relation;
+                //Debug.Log(view.titleText.text + " selected!");
+            }
+        );
+    }
+
+    public void ReadJSON()
+    {
+        string path = Application.streamingAssetsPath + "/Users.dat";
+        string JSONString = File.ReadAllText(path);
+        Users[] user = JsonHelper.FromJson<Users>(JSONString);
+         for (int i = 0; i < user.Length; i++)
+        {
+          userDetail.Add(new Users() { userId = i, name = user[i].name, age = user[i].age, relation = user[i].relation });
+        }
+    }
+
+    public class TestUserView
+    {
+        public Text titleText;
+        public Button clickButton;
+
+        public TestUserView(Transform rootView)
+        {
+            titleText = rootView.Find("TitleText").GetComponent<Text>();
+            clickButton = rootView.Find("ClickButton").GetComponent<Button>();
+        }
+    }
+
+    [System.Serializable]
+    public class Users
+    {
+        public string name;
+        public string age;
+        public string relation;
+        public int userId;
+    }
+
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.users;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.users = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.users = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [System.Serializable]
+        private class Wrapper<T>
+        {
+            public T[] users;
+        }
+    }
+}
