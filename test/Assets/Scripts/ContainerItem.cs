24ef9e9074377eaefb853ff994607ad9053b4470
ilia_sokolov <ilia_sokolov90@mail.ru>
2021-11-13 11:44:05 +0300

    11

--- test/Assets/Scripts/ContainerItem.cs
+++ test/Assets/Scripts/ContainerItem.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ContainerItem : MonoBehaviour
 {
-    public ContainerItem prefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public ContainerItem prefab;
 
+    [SerializeField] private Text initUserName;
+    [SerializeField] private Button clickButton;
+
+
+    public void Init(string payload)
+    {
+        initUserName.text = payload;
+        clickButton.onClick.AddListener(
+        () =>
+        {
+            foreach (var user in GetComponent<ContainerItemScript>().usersDataBaseScript.userDetail)
+            {
+                GetComponent<ContainerItemScript>().details.text = "Name: " + user.name + "\r\nAge: " + user.age + "\r\nRelation: " + user.relation;
+            }
+        }
+        );
+    }
 }
--- test/Assets/Scripts/ContainerItemScript.cs
+++ test/Assets/Scripts/ContainerItemScript.cs
@@ -5,48 +5,29 @@ using UnityEngine.UI;
 
 public class ContainerItemScript : MonoBehaviour
 {
-    public UsersDataBase UsersDataBaseScript;
+    public UsersDataBase usersDataBaseScript;
+   // public ContainerItem containerItemScript;
 
-    [SerializeField] private Text details;
+    public Text details;
     [SerializeField] private RectTransform content;
     [SerializeField] private ContainerItem prefab;
     [SerializeField] private List<ContainerItem> someUser = new List<ContainerItem>();
 
     private void Start()
     {
-        UsersDataBaseScript = GameObject.Find("UsersDataBase").GetComponent<UsersDataBase>();                   
-        prefab = Resources.Load("Assets/Resources/Prefabs/ItemPrefab.prefab", typeof(ContainerItem)) as ContainerItem;
+        usersDataBaseScript = GameObject.Find("UsersDataBase").GetComponent<UsersDataBase>();
+       // containerItemScript = GameObject.Find("Test").GetComponent<ContainerItem>();
+        prefab = Resources.Load("Prefabs/ItemPrefab", typeof(ContainerItem)) as ContainerItem;
         FillItemsViewFromData();
     }
 
     private void FillItemsViewFromData()
     {
-        foreach (var user in UsersDataBaseScript.userDetail)
+        foreach (var user in usersDataBaseScript.userDetail)
         {
             var item = GameObject.Instantiate(prefab, content) as ContainerItem;
             item.Init(user.name);
             someUser.Add(item);         
-        }
-    }
-
-    public class ContainerItem : MonoBehaviour
-    {
-        [SerializeField] private Text initUserName;
-        [SerializeField] private Button clickButton;
-        
-
-        public void Init(string payload)
-        {
-            initUserName.text = payload;
-            clickButton.onClick.AddListener(
-            () =>
-            {
-                foreach (var user in GetComponent<ContainerItemScript>().UsersDataBaseScript.userDetail)
-                {
-                    GetComponent<ContainerItemScript>().details.text = "Name: " + user.name + "\r\nAge: " + user.age + "\r\nRelation: " + user.relation;
-                }
-            }
-            );
         }
     }
 }
